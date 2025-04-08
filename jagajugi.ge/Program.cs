5569785b4c586151ac2695559bc85c2bfefa8d58
irakli1408 <irnikoleishvili@gmail.com>
2025-04-08 19:04:34 +0400

    create v1

--- jagajugi.ge/Program.cs
+++ jagajugi.ge/Program.cs
@@ -1,25 +1,70 @@
+﻿using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapPost("/download-video", (string url) =>
+{
+    if (string.IsNullOrEmpty(url))
+    {
+        return Results.BadRequest("URL is required.");
+    }
 
-app.UseAuthorization();
+    string outputDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
 
-app.MapControllers();
+    Directory.CreateDirectory(outputDirectory);
+
+    string outputTemplate = Path.Combine(outputDirectory, "%(title).128s.%(ext)s");
+
+    Console.WriteLine($"Output will be saved to: {outputTemplate}");
+
+    var process = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = "yt-dlp.exe",
+            Arguments = $"-x --audio-format mp3 --no-mtime --no-cache-dir --force-overwrites --no-post-overwrites -o \"{outputTemplate}\" \"{url}\"",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        }
+    };
+
+
+    //process.OutputDataReceived += (sender, e) =>
+    //{
+    //    if (e.Data != null)
+    //        Console.WriteLine(e.Data);
+    //};
+
+    //process.ErrorDataReceived += (sender, e) =>
+    //{
+    //    if (e.Data != null)
+    //        Console.WriteLine("Error: " + e.Data);
+    //};
+
+    process.Start();
+    process.BeginOutputReadLine();
+    process.BeginErrorReadLine();
+    process.WaitForExit();
+
+    if (process.ExitCode != 0)
+    {
+        return Results.StatusCode(500);
+    }
+
+    return Results.Ok("✅ Done! Check the 'downloads' folder.");
+});
 
 app.Run();
